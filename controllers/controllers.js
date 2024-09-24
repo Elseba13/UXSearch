cc6a1a239038f9e408368e3eb0fd924d001941b6
Elseba13 <114709397+Elseba13@users.noreply.github.com>
2024-09-23 21:23:11 -0300

    controladores ajustados a la base de datos

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,18 +1,13 @@
 const pool = require('../config/db');
 
-// Crear un nuevo método
+
 exports.create = (req, res) => {
-    const { id_metodo, Nombre_Metodo, Resumen_Metodo, Ventajas_Metodo, Desventajas_Metodo, Referencia_Metodo } = req.body;
+    const {Nombre_Metodo, Resumen_Metodo, Ventajas_Metodo, Desventajas_Metodo, Referencia_Metodo } = req.body;
 
-    // Validación: asegurarse de que id_metodo no sea nulo
-    if (!id_metodo || !Nombre_Metodo) {
-        return res.status(400).json({ error: 'El campo id_metodo y Nombre_Metodo son requeridos' });
-    }
+    const query = `INSERT INTO "métodos" ("nombre_metodo", "resumen_metodo", "ventajas_metodo", "desventajas_metodo", "referencia_metodo")
+                   VALUES ($1, $2, $3, $4, $5) RETURNING *`;
 
-    const query = `INSERT INTO "métodos" ("id_metodo", "nombre_metodo", "resumen_metodo", "ventajas_metodo", "desventajas_metodo", "referencia_metodo")
-                   VALUES ($1, $2, $3, $4, $5, $6) RETURNING *`;
-
-    pool.query(query, [id_metodo, Nombre_Metodo, Resumen_Metodo, Ventajas_Metodo, Desventajas_Metodo, Referencia_Metodo], (err, results) => {
+    pool.query(query, [Nombre_Metodo, Resumen_Metodo, Ventajas_Metodo, Desventajas_Metodo, Referencia_Metodo], (err, results) => {
         if (err) {
             console.error('Error:', err);
             return res.status(500).json({ error: 'Error creando el método' });
